e2d07f33a828ad99c1c9343bbd09cc16dfe98593
Mike Coats <mike.coats@nature.scot>
2020-08-14 14:44:29 +0100

    Set some sensible default values for configs

--- src/config.js
+++ src/config.js
@@ -1,14 +1,11 @@
-import assert from 'assert';
-
-// Declare up front what env vars we need to continue and ensure they're set.
-assert(process.env.CSP_PORT !== undefined, 'A port number (CSP_PORT) is required.');
-
+// Grab our config from the env vars, or set some defaults if they're missing.
 const config = Object.freeze({
-  port: process.env.CSP_PORT,
-  hostPrefix: process.env.CSP_HOST_PREFIX
-    ? `${process.env.CSP_HOST_PREFIX}`
-    : `http://localhost:${process.env.CSP_PORT}`,
-  pathPrefix: process.env.CSP_PATH_PREFIX ? `/${process.env.CSP_PATH_PREFIX}` : ''
+  port: process.env.CSP_PORT || '3004',
+  hostPrefix:
+    process.env.CSP_HOST_PREFIX || process.env.CSP_PORT
+      ? `http://localhost:${process.env.CSP_PORT}`
+      : 'http://localhost:3004',
+  pathPrefix: process.env.CSP_PATH_PREFIX ? `/${process.env.CSP_PATH_PREFIX}` : '/cross-service-pages',
 });
 
 export {config as default};
