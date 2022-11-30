3d8a3d84cb4763ee26839008cfaee76f618f67cf
Paul Cox <paul.cox@nature.scot>
2022-11-30 07:54:14 +0000

    Correct describe string on 413 cypress test. Issue Licensing 1452

--- cypress/integration/003-error-413.js
+++ cypress/integration/003-error-413.js
@@ -1,4 +1,4 @@
-describe('Internal server error page', function () {
+describe('request entity too large error page', function () {
   it('successfully loads', function () {
     cy.visit('/error-413', {failOnStatusCode: false});
     cy.get('h1').should('contain', 'Maximum permitted file size exceeded');
