67d63014fe34cf15a1584b600185b8c88bc7d65a
yatjam <yates_7@hotmail.com>
2025-07-16 13:23:12 +0100

    Remove back link
    Issue: Scottish-Natural-Heritage/Licensing#2868

--- src/assets/back.js
+++ src/assets/back.js
@@ -1,12 +1,12 @@
-// Event listener listening for back link clicks.
-document.querySelector('#back-link').addEventListener('click', goBack);
+// // Event listener listening for back link clicks.
+// document.querySelector('#back-link').addEventListener('click', goBack);
 
-/**
- * Function to go backwards through page history.
- *
- * @returns {boolean} Always returns false, needed for Chrome.
- */
-function goBack() {
-  history.go(-1);
-  return false;
-}
+// /**
+//  * Function to go backwards through page history.
+//  *
+//  * @returns {boolean} Always returns false, needed for Chrome.
+//  */
+// function goBack() {
+//   history.go(-1);
+//   return false;
+// }
