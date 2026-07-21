29b994466ddacf8fd3006cf4f4c0cd0a8263aea0
yatjam <yates_7@hotmail.com>
2026-07-21 12:54:21 +0100

    Update hostPrefix to remove always resolving to localhost.
    Issue: Scottish-Natural-Heritage/Licensing#3502

--- src/config.js
+++ src/config.js
@@ -1,12 +1,10 @@
 import process from 'node:process';
 
+const port = process.env.CSP_PORT || '3004';
 // Grab our config from the env vars, or set some defaults if they're missing.
 const config = Object.freeze({
-  port: process.env.CSP_PORT || '3004',
-  hostPrefix:
-    process.env.CSP_HOST_PREFIX || process.env.CSP_PORT
-      ? `http://localhost:${process.env.CSP_PORT}`
-      : 'http://localhost:3004',
+  port,
+  hostPrefix: process.env.CSP_HOST_PREFIX || `http://localhost:${port}`,
   pathPrefix: process.env.CSP_PATH_PREFIX ? `/${process.env.CSP_PATH_PREFIX}` : '/cross-service-pages'
 });
 
